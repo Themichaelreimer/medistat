f6e9e38260f54f2a59b63d4cc72343ddd35f3484
themichaelreimer <themichaelreimer@gmail.com>
2023-04-05 15:30:37 -0500

    lint

--- commands/start_reverse_proxy.py
+++ commands/start_reverse_proxy.py
@@ -5,8 +5,8 @@ from .common.docker_helpers import detect_docker_compose_command
 
 def run():
     command = detect_docker_compose_command()
-    use_ssl = os.environ.get('SSL') == 'true'
-    file = 'docker-compose-traefik-ssl.yml' if use_ssl else 'docker-compose-traefik.yml'
+    use_ssl = os.environ.get("SSL") == "true"
+    file = "docker-compose-traefik-ssl.yml" if use_ssl else "docker-compose-traefik.yml"
 
     # TODO: Consider if this is a systemd service
     daemon_flag = "-d" if not ("--terminal" in sys.argv or "-t" in sys.argv) else ""
