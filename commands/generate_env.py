e911e3884c72410283934c064be1ce41b7bfeaba
themichaelreimer <themichaelreimer@gmail.com>
2023-04-14 10:01:45 -0500

    explicit frontend host

--- commands/generate_env.py
+++ commands/generate_env.py
@@ -16,6 +16,7 @@ def run() -> None:
     pr_name = sys.argv[1].lower()
     sample_env = read_sample_env_file()
     sample_env = replace_host(sample_env, pr_name)
+    sample_env = replace_frontend_host(sample_env, pr_name)
     sample_env = replace_project_name(sample_env, pr_name)
     sample_env = replace_ssl_status(sample_env)
 
@@ -39,6 +40,10 @@ def write_env_file(file_contents: str) -> None:
 
 def replace_host(env_file_contents: str, pr_name: str) -> str:
     return env_file_contents.replace("HOST=localhost", f"HOST=medistat.online")
+
+
+def replace_frontend_host(env_file_contents: str, pr_name: str) -> str:
+    return env_file_contents.replace("FRONTEND_HOST=medistat.online", f"URL_PREFIX={pr_name}.medistat.online")
 
 
 def replace_project_name(env_file_contents: str, pr_name: str) -> str:
